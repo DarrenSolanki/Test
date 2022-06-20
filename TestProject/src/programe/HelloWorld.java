012812333fbe2af45bf6697950e5e78310ad0a4b
Dharm <Dharm@DESKTOP-73BS8PI>
2022-06-20 22:34:27 +0530

    2nd

--- TestProject/src/programe/HelloWorld.java
+++ TestProject/src/programe/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		System.out.println("Hello wor");
 		System.out.println("3+2 is : "+(3+2));
 	}
 
