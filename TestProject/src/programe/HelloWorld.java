b59c0c71f45c1cf21e65836e5fd2d7ddf022c1cb
Dharm <Dharm@DESKTOP-73BS8PI>
2022-06-20 22:28:38 +0530

    first commit

---
+++ TestProject/src/programe/HelloWorld.java
@@ -0,0 +1,11 @@
+package programe;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		System.out.println("3+2 is : "+(3+2));
+	}
+
+}
